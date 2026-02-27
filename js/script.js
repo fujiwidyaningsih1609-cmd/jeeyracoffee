f58a701955941abeed5d0d75e303ee149d50216d
fujiwidyaningsih1609-cmd <fujiwidyaningsih1609@gmail.com>
2026-02-27 08:50:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Toggle class active untuk hamburger menu
+const navbarMenu = document.querySelector(".navbar-menu");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarMenu.classList.toggle("active");
+};
+s;
